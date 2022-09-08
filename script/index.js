35e2a5ef589b58b553c0d77a531e6b55003e64da
Yehezkiel David Setiawan <yehezkieldavid2006@gmail.com>
2022-09-08 14:39:57 +0700

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -3,17 +3,6 @@ function showAlert() {
   alert('Nama Akun : admin \nKata Sandi : admin123 \n \nNama Akun : user\nKata Sandi : user123');
 }
 
-// toggle password
-const togglePassword = document.querySelector('#togglePassword');
-const password = document.querySelector('#pswrd');
-const login_form = document.querySelector('#login-form');
-
-togglePassword.addEventListener('click', function (e) {
-  const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-
-  password.setAttribute('type', type);
-});
-
 // verifikasi akun admin
 
 function acc() {
